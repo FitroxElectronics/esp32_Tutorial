18cea948fa943adcf81e303eaecf0395268a40e8
Fitrox Electronics <33017282+FitroxElectronics@users.noreply.github.com>
2019-08-23 07:56:35 +0700

    Create esp32_ble_1.ino

---
+++ ESP32_C8_BLE/esp32_ble_1/esp32_ble_1.ino
@@ -0,0 +1,47 @@
+// esp32 chapter8 exm.1
+// BLE server
+
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+// กำหนด service UUID และ characteristic UUID
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  // กำหนดชื่ออุปกรณ์ของเราเป็นชื่อ MyESP32
+  BLEDevice::init("MyESP32");
+
+  // ทำการสร้าง BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+// ให้แสดงประโยค "Hello World says Neil"
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+}
